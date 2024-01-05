ae6b48f84a6d0e15d0c809bbb82224d0d12e3c3b
RoniPick <ronipick3@gmail.com>
2024-01-05 14:08:40 +0200

    Retreving data from API

--- remoteok_scraper.py
+++ remoteok_scraper.py
@@ -8,5 +8,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+BASE_URL = 'https://remoteok.io/api'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Crome/96.0.4664.45 Safari/537.36' 
+REQUEST_HEADER = {'User-Agent': USER_AGENT,  # Device and browser details
+                  'Accept-Language': 'en-US,en;q=0.5'} # Language details
+
+
+def get_job_postings():
+    res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
+    return res.json() 
+
 if __name__ == "__main__":
-    print("Hello World")
+    json = get_job_postings()[1]
+    print(json)
