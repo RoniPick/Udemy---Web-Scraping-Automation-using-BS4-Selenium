79999fa19e475f6d8af6166656b5159b55758228
RoniPick <ronipick3@gmail.com>
2024-01-06 16:49:30 +0200

    Configuring Python Selenium driver

--- web_bot/trello_bot.py
+++ web_bot/trello_bot.py
@@ -7,9 +7,22 @@ from datetime import datetime
 import os
 import json
 
+CHROME_DRIVER_PATH = os.path.join(os.getcwd(), "chromedriver")
+OP = webdriver.ChromeOptions()
+# OP.add_argument("--headless")
+OP.add_argument("--no-sandbox")  # Add this line if running in a Linux environment
+
+
+DRIVER = webdriver.Chrome(options=OP)
 
 def main():
-    pass
+    try:
+        DRIVER.get("https://trello.com")
+        input("Bot operation complete. Press enter to exit.")
+        DRIVER.close()
+    except Exception as e:
+        print(e)
+        DRIVER.close()
 
 if __name__ == "__main__":
     main()
