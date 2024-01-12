cc7914b2e164cd102fc07688222ff0834ed7b836
RoniPick <ronipick3@gmail.com>
2024-01-12 14:02:32 +0200

    Adding task to the To Do list

--- web_bot/trello_bot.py
+++ web_bot/trello_bot.py
@@ -10,6 +10,7 @@ import json
 CHROME_DRIVER_PATH = os.path.join(os.getcwd(), "chromedriver")
 OP = webdriver.ChromeOptions()
 NUMBER = 1
+TASK_NUMBER = 1
 # OP.add_argument("--headless")
 OP.add_argument("--no-sandbox")  # Add this line if running in a Linux environment
 
@@ -19,7 +20,6 @@ def username(config):
     DRIVER.find_element(By.XPATH, value="//*[@id='BXP-APP']/header[1]/div/div[1]/div[2]/a[1]").click() # Find the login button, XPATH help to find any element on the page
     time.sleep(5)
     Username = DRIVER.find_element(By.ID, value="username") # Find the username field
-    time.sleep(2)
     Username.clear()
     Username.send_keys(config["USERNAME"]) # Enter the username
 
@@ -31,12 +31,12 @@ def password(config):
     password.send_keys(config["PASSWORD"])
 
 def create_board():
-    global NUMBER  # Add this line to declare NUMBER as a global variable
+    global NUMBER  # declaring NUMBER as a global variable
     DRIVER.find_element(By.XPATH, value="//div[@class='board-tile mod-add']").click()
     time.sleep(2)
     board_name = DRIVER.find_element(By.XPATH, value="//input[contains(@class,'nch-textfield__input lsOhPsHuxEMYEb')]")
     board_name.clear()
-    board_name.send_keys("Test Board" + str(NUMBER))
+    board_name.send_keys("Test Board " + str(NUMBER))
     NUMBER += 1 # Increment the number of boards created
     DRIVER.find_element(By.XPATH, value="//button[contains(@class,'hY6kPzdkHFJhfG bxgKMAm3lq5BpA')]").click()
     time.sleep(2)
@@ -45,6 +45,15 @@ def navigate_to_board():
     DRIVER.find_element(By.XPATH, value="//a[.='Test Board']").click()
     time.sleep(2)
 
+def add_task():
+    global TASK_NUMBER  # declaring TASK_NUMBER as a global variable
+    DRIVER.find_element(By.XPATH, value="(//button[contains(@class,'O9vivwyDxMqo3q bxgKMAm3lq5BpA')])[1]").click()
+    task_name = DRIVER.find_element(By.XPATH, value="//textarea[@data-testid='list-card-composer-textarea']")
+    time.sleep(1)
+    task_name.clear()
+    task_name.send_keys("Test Task " + str(TASK_NUMBER))
+    TASK_NUMBER += 1 # Increment the number of tasks created
+    DRIVER.find_element(By.XPATH, value="//button[@data-testid='list-card-composer-add-card-button']").click()
 
 def login():
     with open('config.json') as config_file:
@@ -62,6 +71,7 @@ def main():
         login()
          # create_board()
         navigate_to_board()
+        add_task()
         input("Bot operation complete. Press enter to exit.")
         DRIVER.close()
     except Exception as e:
